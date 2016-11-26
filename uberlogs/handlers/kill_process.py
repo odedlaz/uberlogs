82f8861df01d67335499682743f69b1763cc3c35
odedlaz <odedlaz@gmail.com>
2016-11-26 18:08:35 +0200

    Remove redundant try/catch block in kill process handler

--- uberlogs/handlers/kill_process.py
+++ uberlogs/handlers/kill_process.py
@@ -9,14 +9,13 @@ class KillProcessHandler(LoggingHandler):
         if record.levelno != self.level:
             return
 
-        try:
-            # flush text before exiting
-            for fd in [sys.stdout, sys.stderr]:
-                fd.flush()
-        finally:
-            # Twisted writes unhandled errors in different calls
-            # If we exit on the first call, we'd lose the actual error
-            for log_to_ignore in ["Unhandled error in Deferred"]:
-                if log_to_ignore.lower() in record.getMessage().lower():
-                    return
-            os._exit(1)
+        # flush text before exiting
+        for fd in [sys.stdout, sys.stderr]:
+            fd.flush()
+
+        # Twisted writes unhandled errors in different calls
+        # If we exit on the first call, we'd lose the actual error
+        for log_to_ignore in ["Unhandled error in Deferred"]:
+            if log_to_ignore.lower() in record.getMessage().lower():
+                return
+        os._exit(1)
