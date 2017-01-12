11d22df993b99752783747e386ab70fcf53bf5d0
odedlaz <odedlaz@users.noreply.github.com>
2017-01-12 12:13:00 +0200

    Update level.py

--- uberlogs/level.py
+++ uberlogs/level.py
@@ -1,1 +1,15 @@
 from logging import DEBUG, INFO, WARN, WARNING, ERROR, CRITICAL, FATAL, NOTSET
+
+
+NAME_TO_VALUE = { "DEBUG": DEBUG,
+                  "INFO": INFO,
+                  "WARN": WARN,
+                  "WARNING": WARNING,
+                  "ERROR": ERROR,
+                  "CRITICAL": CRITICAL,
+                  "FATAL": FATAL
+                }
+
+VALUE_TO_NAME = { v:k for k,v in NAME_TO_VALUE.items() }
+
+ALL = VALUE_TO_NAME.keys()
