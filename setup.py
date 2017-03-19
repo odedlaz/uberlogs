ad8251ab08dd3f7a396c2f7dc8daa23291174c72
Oded Lazar <odedlaz@gmail.com>
2017-03-19 14:14:14 +0200

    Bump version: 0.0.13 → 0.0.14

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class PyTest(TestCommand):
 
 setup(
     name='uberlogs',
-    version='0.0.13',
+    version='0.0.14',
     url='http://github.com/odedlaz/uberlogs',
     license='MIT License',
     author='Oded Lazar',
